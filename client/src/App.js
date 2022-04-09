4011964657dc6ddca84aeef46f48ec938d5fd8f8
besparza <esparzabrian90@gmail.com>
2022-04-09 16:58:55 -0700

    created login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import Home from './components/Home';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 import Section3 from './components/Section3';
-
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
     <Section1/>
     <Section2/>
     <Section3/>
-    
+    <Login/>
+    <Register/>
     </>
   );
 }
