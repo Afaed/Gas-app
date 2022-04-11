5bb1fdc7829c25cf446119c714719c7a2f49362b
besparza <esparzabrian90@gmail.com>
2022-04-11 13:00:41 -0700

    added register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
-import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './Pages/Home';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+    </Router>
+  )
+}
+
+/*function App() {
   return (
     <>
    
@@ -21,5 +33,5 @@ function App() {
     </>
   );
 }
-
+*/
 export default App;
