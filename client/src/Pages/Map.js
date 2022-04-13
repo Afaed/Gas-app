2cd8e57bbbead9adbce8ad809a48906f974cf69b
Daniel <ddoherty6@gmail.com>
2022-04-13 14:50:10 -0700

    adding map features

--- client/src/Pages/Map.js
+++ client/src/Pages/Map.js
@@ -1,39 +1,159 @@
 import React, { useEffect, useRef, useState } from 'react';
 import {GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-let userLocation = {};
+const sampleObj = require('../utils/sample-obj');
+let gsMarkers = JSON.parse(sampleObj);
 
-function setCurrentPosition( position ) { 
+function getStations(endpoint) {
+    // fetch(endpoint)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         console.log(data.results);
+    //         console.log(data.results.items);
+        
+    //         for (let i = 0; i < data.results.items.length; i++) {
+    //             gsMarkers.push({
+    //                 id: data.results.items[i].id,
+    //                 location: data.results.items[i].position,
+    //                 name: data.results.items[i].title,
+    //                 address: data.results.items[i].vicinity
+    //             })
+    //         }
 
-    userLocation = {lat: position.coords.latitude, lng: position.coords.longitude };
+    //         console.log(gsMarkers);
+    //         console.log(JSON.stringify(gsMarkers));
+
+            
+    //     })
+    
+   
+   
+        // .then(res => res.json())
+        // .then(data => {
+        //     fs.writeFileSync('../utils/data.json', JSON.stringify(data, null, 2), 'utf-8');
+        // });
+
+    
+
+
+
+
+    //console.log(sampleObj);
+    //console.log(sampleObj.replace(/'/g, '"'));
+    // gasStations = JSON.parse(sampleObj.replace(/'/g, '"'));
+    // console.log(sampleObj);
+
 }
 
-function positionError( error ) { 
 
-    console.log(error);
+function nearbyGsEndPoint(lat, lng) {
+    return `https://places.ls.hereapi.com/places/v1/discover/explore?at=${lat}%2C${lng}&cat=petrol-station&apiKey=dD36DAdimAzcwz4H84i4jX-g-nAxGa8xZGmtD3nFRvw`;
 }
 
-navigator.geolocation.getCurrentPosition(setCurrentPosition, positionError);
+// function setCurrentPosition( position ) { 
+
+//     userLocation = {lat: position.coords.latitude, lng: position.coords.longitude };
+// }
+
+// function positionError( error ) { 
+
+//     console.log(error);
+// }
+
+// navigator.geolocation.getCurrentPosition(setCurrentPosition, positionError);
+
+// fetch('http://ip-api.com/json')
+//     .then(res => res.json())
+//     .then(data => {
+        
+//     })
+// ;
 
 const NestedMapDependencies = () => {
 
+    // let [userLocation, setUserLocation] = useState(false);
+    // let [gsData, setGsData] = useState(false);
+    const [mapData, setMapData] = useState(false);
+
+    async function fetchFromAPIs() {
+        //return await fetch('http://ip-api.com/json'); // get user location
+    
+        try {
+            const userLocationRes = await fetch('http://ip-api.com/json');
+            const userLocationData = userLocationRes.json();
+            const userLocation = {
+                userCoords: {
+                    lat: userLocationData.lat,
+                    lng: userLocationData.lon
+                }
+            }
+    
+            const gsLocationsRes = await fetch(nearbyGsEndPoint(userLocationData.lat, userLocationData.lon));
+            const gsLocationsData = gsLocationsRes.json();
+            const gsLocations = gsLocationsData.results.items;
+            let gsMarkers = [];
+    
+            for (let i = 0; i < gsLocations.length; i++) {
+                gsMarkers.push({
+                    id: gsLocations[i].id,
+                    location: gsLocations[i].position,
+                    name: gsLocations[i].title,
+                    address: gsLocations[i].vicinity
+                })
+            }
+    
+            return({
+                ...userLocation,
+                gsMarkers: gsMarkers
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+
+    useEffect(() => {
+        
+        console.log(mapData);
+
+        if(!mapData) { 
+
+            // const dataForMap = fetchFromAPIs();
+
+            const userLocation = {
+                    lat: 37.7893403,
+                    lng: -122.2267357
+            }
+
+            const mockObj = {
+                userLocation,
+                gsMarkers
+            }
+
+            console.log(mockObj);
+
+            setMapData(mockObj);
+            
+        }        
+    }, [])
+
     return <GoogleMap 
-            zoom={10} 
-            center={userLocation}
+            zoom={8} 
+            center={mapData.userLocation}
             mapContainerClassName="map-container"
         >
-            <Marker position={userLocation} />
+            {mapData.gsMarkers.map((station) => (
+                <Marker key={station.id} position={{lat: station.location[0], lng: station.location[1]}} />
+            ))}
+            <Marker position={mapData.userLocation} />
         </GoogleMap>;
 
 }
 
 const Map = () => {
 
-    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
-
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyBRm3itj6oBIld55_PLJzQ6noL1aF3w_Uk"
-        //"AIzaSyBRm3itj6oBIld55_PLJzQ6noL1aF3w_Uk"
+        googleMapsApiKey: "AIzaSyC2A-PNzqQ25vdOH13qcLe2pMkjZZUiwjo"
     });
 
     if (!isLoaded) return <div>Loading...</div>;
