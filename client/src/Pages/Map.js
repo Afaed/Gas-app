ecb2afc78470db4c8eb3a7311efbd90fee198844
klem <ddoherty6@gmail.com>
2022-04-13 17:14:08 -0700

    hardcoded map for demo

--- client/src/Pages/Map.js
+++ client/src/Pages/Map.js
@@ -45,6 +45,42 @@ function getStations(endpoint) {
 
 }
 
+async function fetchFromAPIs() {
+    //return await fetch('http://ip-api.com/json'); // get user location
+
+    try {
+        const userLocationRes = await fetch('http://ip-api.com/json');
+        const userLocationData = userLocationRes.json();
+        const userLocation = {
+            userCoords: {
+                lat: userLocationData.lat,
+                lng: userLocationData.lon
+            }
+        }
+
+        const gsLocationsRes = await fetch(nearbyGsEndPoint(userLocationData.lat, userLocationData.lon));
+        const gsLocationsData = gsLocationsRes.json();
+        const gsLocations = gsLocationsData.results.items;
+        let gsMarkers = [];
+
+        for (let i = 0; i < gsLocations.length; i++) {
+            gsMarkers.push({
+                id: gsLocations[i].id,
+                location: gsLocations[i].position,
+                name: gsLocations[i].title,
+                address: gsLocations[i].vicinity
+            })
+        }
+
+        return({
+            ...userLocation,
+            gsMarkers: gsMarkers
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 function nearbyGsEndPoint(lat, lng) {
     return `https://places.ls.hereapi.com/places/v1/discover/explore?at=${lat}%2C${lng}&cat=petrol-station&apiKey=dD36DAdimAzcwz4H84i4jX-g-nAxGa8xZGmtD3nFRvw`;
@@ -71,79 +107,25 @@ function nearbyGsEndPoint(lat, lng) {
 
 const NestedMapDependencies = () => {
 
-    // let [userLocation, setUserLocation] = useState(false);
-    // let [gsData, setGsData] = useState(false);
-    const [mapData, setMapData] = useState(false);
-
-    async function fetchFromAPIs() {
-        //return await fetch('http://ip-api.com/json'); // get user location
-    
-        try {
-            const userLocationRes = await fetch('http://ip-api.com/json');
-            const userLocationData = userLocationRes.json();
-            const userLocation = {
-                userCoords: {
-                    lat: userLocationData.lat,
-                    lng: userLocationData.lon
-                }
-            }
-    
-            const gsLocationsRes = await fetch(nearbyGsEndPoint(userLocationData.lat, userLocationData.lon));
-            const gsLocationsData = gsLocationsRes.json();
-            const gsLocations = gsLocationsData.results.items;
-            let gsMarkers = [];
-    
-            for (let i = 0; i < gsLocations.length; i++) {
-                gsMarkers.push({
-                    id: gsLocations[i].id,
-                    location: gsLocations[i].position,
-                    name: gsLocations[i].title,
-                    address: gsLocations[i].vicinity
-                })
-            }
-    
-            return({
-                ...userLocation,
-                gsMarkers: gsMarkers
-            });
-        } catch (error) {
-            console.log(error);
-        }
+    const userLocation = {
+        lat: 37.7893403,
+        lng: -122.2267357
     }
-    
 
-    useEffect(() => {
-        
-        console.log(mapData);
-
-        if(!mapData) { 
-
-            // const dataForMap = fetchFromAPIs();
-
-            const userLocation = {
-                    lat: 37.7893403,
-                    lng: -122.2267357
-            }
-
-            const mockObj = {
-                userLocation,
-                gsMarkers
-            }
-
-            console.log(mockObj);
-
-            setMapData(mockObj);
-            
-        }        
-    }, [])
+    const mockObj = {
+        userLocation,
+        gsMarkers
+    }
+  
+    const [mapData, setMapData] = useState(mockObj);
 
     return <GoogleMap 
-            zoom={8} 
+            zoom={13} 
             center={mapData.userLocation}
             mapContainerClassName="map-container"
         >
             {mapData.gsMarkers.map((station) => (
-                <Marker key={station.id} position={{lat: station.location[0], lng: station.location[1]}} />
+                <Marker key={station.id} position={{lat: station.location[0], lng: station.location[1]}} icon={'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'} />
             ))}
             <Marker position={mapData.userLocation} />
         </GoogleMap>;
@@ -153,7 +135,7 @@ const NestedMapDependencies = () => {
 const Map = () => {
 
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyC2A-PNzqQ25vdOH13qcLe2pMkjZZUiwjo"
+        googleMapsApiKey: "AIzaSyDx-i0lS1O5SGCiWqCG7kTlGxdKyyRu0Dc"
     });
 
     if (!isLoaded) return <div>Loading...</div>;
